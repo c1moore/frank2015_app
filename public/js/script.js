1c30ab83efa6ec63bed8fd27a87fe9b3b8ec192c
Calvin Moore <c1moore@ufl.edu>
2015-01-27 15:32:30 -0500

    Added Speaker frankbook to sidemenu and changed 'frank book' to 'frankbook' on sidebar.

--- public/js/script.js
+++ public/js/script.js
@@ -86,7 +86,8 @@ var appWindow = angular.module('appWindow', ['snap']);
 			restrict : "E",
 			template : 	"<a href='directory.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-home'></i> Home</a> <br />" +
 						"<a href='frankbook_directory.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-user'></i> Who's Here?</a> <br />" +
-						"<a href='frankbook.html' target='_self' class='btn btn-sidebar sidebar-item sidebar-item-2' role='button'><i class='fa fa-user'></i> frank book</a> <br />" +
+						"<a href='frankbook.html' target='_self' class='btn btn-sidebar sidebar-item sidebar-item-2' role='button'><i class='fa fa-user'></i> frankbook</a> <br />" +
+						"<a href='frankbook_speaker.html' target='_self' class='btn btn-sidebar sidebar-item sidebar-item-2' role='button'><i class='fa fa-microphone'></i> Speaker frankbook</a> <br />" +
 						"<a href='map.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-map-marker'></i> Map</a> <br />" +
 						"<a href='vote.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-check-square-o'></i> Voting</a> <br />",
 			link : function postlink() {
