3a4fd66b77e23129767e7197890680baf400ace1
Calvin Moore <c1moore@ufl.edu>
2015-02-01 15:31:32 -0500

    Positioning, width, and height are all now working as expected.  Work on the time-bar has begun.

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -12,9 +12,12 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 		$scope.storage = localStorageService;
 		$scope.intServArr = interestsService.interests;
 		$scope.eventColors = ["#73C92D", "#F7B518", "#C54E90", "#F7B518"];	//The order in which to use the frank colors.
-		$scope.startDate = new Date(2015, 1, 24);									//Date frank 2015 begins.
+		$scope.startDate = Date.parse(new Date(2015, 1, 24));									//Date frank 2015 begins.
 		$scope.duration = 4;														//The number of days the event lasts.
 		$scope.millisInDay = 1000 * 60 * 60 * 24;
+		$scope.pixelMinuteRatio = 2;
+		$scope.hourlyLeftPos = (25 *2) + (10*2) + 10;	//The size of 2 all-day events (there should be the max # of all day events) plus their left-margin (of 10 pixels) plus the left-margin for the hourly event.
+		$scope.hourlyWidth = $window.innerWidth - $scope.hourlyLeftPos - 55;	//The entire width of the screen minus the width of all-day events (times two as there should not be more than 2) minus the left-margin for all-day events (time 2 as there should not be more than 2) minus the margin between all-day and hourly events minus the right-margin of hourly events (including the scrollbar).
 
 		//Check if the user is logged in.  If not, they should be redirected to the login page.
 		/*var user_id = $scope.storage.get('user_id'),
@@ -35,16 +38,69 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 			$window.location.href = 'login.html';
 		}*/
 
+		$scope.getTopMargin = function(height) {
+			var margin = parseInt(angular.element("#header").css('height'), 10) + height;
+			return margin + "px";
+		};
+
+		/**
+		* Height for the ul element.  It should be as large as the visible screen.
+		*/
+		$scope.slideHeight = function(topMargin) {
+			return ($window.innerHeight - parseInt(angular.element("#header").css('height'), 10) - topMargin) + 'px';
+		};
+
 		$scope.getEventColor = function(index) {
 			return $scope.eventColors[index % $scope.eventColors.length];
-		}
+		};
+
+		/**
+		* Determine whether the time, name, and location should all stay on the same line or if they can fit
+		* on separate lines.  To speed up calculations, the start_time and end_time for the previous event that
+		* called this function will be stored.  If that same event calls this function again (as determined by
+		* the start_time and end_time), the same value can be returned.
+		*/
+		var prevStartTime = null;
+		var prevEndTime = null;
+		var prevReturnedDisplay = null;
+		$scope.getDisplay = function(starttime, endtime) {
+			if(starttime === prevStartTime && endtime === prevEndTime) {
+				return prevReturnedDisplay;
+			}
+
+			prevStartTime = starttime;
+			prevEndTime = endtime;
+
+			if(parseInt($scope.getEventHeight(starttime, endtime), 10) > 85) {
+				prevReturnedDisplay = 'block';
+			} else {
+				prevReturnedDisplay = 'inline-block';
+			}
+
+			return prevReturnedDisplay;
+		};
+
+		/**
+		* Return the distance down the page this event should appear.  To find this, I will use an imaginary
+		* grid with the origin in the top left corner of the beginning of the calendar area that corresponds
+		* to midnight.  The number of milliseconds from the epoch to the given day will be substracted so
+		* only the time that will elapse during the current day remains:
+		*
+		*		startTime - (startDate + (millisecondsInDay * numberOfDayIntoEvent))
+		*
+		* This result will then be converted into minutes (by dividing by 60000) and multiplied by the pixels
+		* to minute ratio.
+		*/
+		$scope.getScrollTop = function(starttime, index) {
+			return (((parseInt(starttime, 10) - ($scope.startDate + ($scope.millisInDay * parseInt(index, 10)))) / (60 * 1000)) * $scope.pixelMinuteRatio) + 'px';
+		};
 
 		/**
 		* Determine the height of the event based on the duration of the event.  starttime and endtime
 		* should be in milliseconds.  1 minute = 3 pixels.
 		*/
 		$scope.getEventHeight = function(starttime, endtime) {
-			return (((endtime - starttime) / (60 * 1000)) * 3) + 'px';
+			return (((endtime - starttime) / (60 * 1000)) * $scope.pixelMinuteRatio) + 'px';
 		};
 
 		/**
@@ -81,7 +137,7 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 		* Returns the number of days into the frank 2015.
 		*/
 		$scope.daysSinceStart = function() {
-			var x = (Date.now() - Date.parse($scope.startDate)) / $scope.millisInDay;
+			var x = (Date.now() - $scope.startDate) / $scope.millisInDay;
 			return (x>=0) ? x : 0;
 		};
 
@@ -100,11 +156,13 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 		*/
 		$scope.segregateDays = function(diverseArr) {
 			$scope.dailyCalendar = [];
-			var day = {};
-			day.allDayList = [];
-			day.hourlyList = [];
+			var day;
 
 			for(var j=0; j<$scope.duration; j++) {
+				day = {};
+				day.allDayList = [];
+				day.hourlyList = [];
+
 				for(var i=0; i<diverseArr.length; i++) {
 					if((diverseArr[i].start_time > getStartOfDay(j)) && (diverseArr[i].start_time < getEndOfDay(j))) {
 						if(diverseArr[i].type === "allday") {
@@ -113,6 +171,7 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 							day.hourlyList.push(diverseArr[i]);
 						}
 						diverseArr.splice(i, 1);
+						i--;
 					}
 				}
 
@@ -139,7 +198,7 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 ]);
 
 frankAgenda.filter('millisToTime', function() {
-	var getTime = function(time) {
+	return function(time) {
 		var date = new Date(time);
 		var hour = date.getHours();
 		var minutes = date.getMinutes();
@@ -147,14 +206,17 @@ frankAgenda.filter('millisToTime', function() {
 
 		if(hour <= 11) {
 			meridian = 'am';
-			hour++;
 		} else {
 			meridian = 'pm';
-			hour = (hour % 12) + 1;
+			hour = (hour % 12);
 		}
+
+		if(!hour)
+			hour = 12;
+
+		if(!minutes)
+			minutes = '00';
 
 		return hour + ':' + minutes + meridian;
 	};
-
-	return getTime;
 });
