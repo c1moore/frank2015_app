49f3a14d5330d1645a503d2a4bf7c251b3cc3d24
Calvin Moore <c1moore@ufl.edu>
2015-01-31 23:08:30 -0500

    Time spans are now getting the correct background color and height.

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -11,8 +11,9 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 	function($scope, $http, localStorageService, $window, interestsService, $timeout) {
 		$scope.storage = localStorageService;
 		$scope.intServArr = interestsService.interests;
-		$scope.eventColors = ['#73C92D', '#F7B518', '#C54E90', '#F7B518'];
-		$scope.startDate = new Date(2015, 1, 24);	//Date frank 2015 begins.
+		$scope.eventColors = ["#73C92D", "#F7B518", "#C54E90", "#F7B518"];	//The order in which to use the frank colors.
+		$scope.startDate = new Date(2015, 1, 24);									//Date frank 2015 begins.
+		$scope.duration = 4;														//The number of days the event lasts.
 		$scope.millisInDay = 1000 * 60 * 60 * 24;
 
 		//Check if the user is logged in.  If not, they should be redirected to the login page.
@@ -34,12 +35,16 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 			$window.location.href = 'login.html';
 		}*/
 
+		$scope.getEventColor = function(index) {
+			return $scope.eventColors[index % $scope.eventColors.length];
+		}
+
 		/**
 		* Determine the height of the event based on the duration of the event.  starttime and endtime
 		* should be in milliseconds.  1 minute = 3 pixels.
 		*/
-		var getHeight = function(starttime, endtime) {
-			return ((endtime - starttime) / 60) * 3;
+		$scope.getEventHeight = function(starttime, endtime) {
+			return (((endtime - starttime) / (60 * 1000)) * 3) + 'px';
 		};
 
 		/**
@@ -53,17 +58,17 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 		};
 
 		/**
-		* Return the time for the start of the day in milliseconds.
+		* Return the time for the start of the indexth day in milliseconds.
 		*/
 		var getStartOfDay = function(index) {
 			var now = new Date($scope.startDate + ($scope.millisInDay * index));
-			var today = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
+			var today = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
 
 			return Date.parse(today);
-		}
+		};
 
 		/**
-		* Return the time for the end of the day in milliseconds.
+		* Return the time for the end of the indexth day in milliseconds.
 		*/
 		var getEndOfDay = function(index) {
 			var now = new Date($scope.startDate + ($scope.millisInDay * index));
@@ -71,6 +76,16 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 
 			return Date.parse(today);
 		};
+
+		/**
+		* Returns the number of days into the frank 2015.
+		*/
+		$scope.daysSinceStart = function() {
+			var x = (Date.now() - Date.parse($scope.startDate)) / $scope.millisInDay;
+			return (x>=0) ? x : 0;
+		};
+
+		$scope.day = $scope.daysSinceStart();		//Set the index for the carousel.
 
 		/**
 		* Determines if a given event occurs today.  Since events do not go over to the next day, only
@@ -81,17 +96,42 @@ frankAgenda.controller('agendaController', ['$scope', '$http', 'localStorageServ
 		};
 
 		/**
+		* Reorganize returned data to separate events into separate days.
+		*/
+		$scope.segregateDays = function(diverseArr) {
+			$scope.dailyCalendar = [];
+			var day = {};
+			day.allDayList = [];
+			day.hourlyList = [];
+
+			for(var j=0; j<$scope.duration; j++) {
+				for(var i=0; i<diverseArr.length; i++) {
+					if((diverseArr[i].start_time > getStartOfDay(j)) && (diverseArr[i].start_time < getEndOfDay(j))) {
+						if(diverseArr[i].type === "allday") {
+							day.allDayList.push(diverseArr[i]);
+						} else {
+							day.hourlyList.push(diverseArr[i]);
+						}
+						diverseArr.splice(i, 1);
+					}
+				}
+
+				$scope.dailyCalendar.push(day);
+			}
+		};
+
+		/**
 		* To speed up retreiving the calendar from the server, first only get events that start today
 		* or later.  After this data has been retreived and manipulated, load the data for previous
 		* days, manipulate it, and add it to the array.
 		*/
 		$scope.dailyCalendar = [];
-		$http.post('../../app/controllers/get_agenda.php', {'start_date' : getMidnightTime()}).success(function(response) {
-			$scope.dailyCalendar = response;
+		$http.post('../../app/controllers/get_agenda.php', {'start_date' : 0}).success(function(response) {
+			$scope.segregateDays(response);
 
-			$http.post('../../app/controllers/get_agenda.php', {'end_date' : getMidnightTime()}).success(function(response) {
-				$scope.dailyCalendar.unshift(response.slice());
-			});
+			/*$http.post('../../app/controllers/get_agenda.php', {'end_date' : getMidnightTime()}).success(function(response) {
+				segregateDays(response);
+			});*/
 		}).error(function(response, status) {
 			$window.alert("There was an error connecting to the servers (possibly due to all the cool people here).  Please try again later.");
 		});
