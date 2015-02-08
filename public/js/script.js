defa6d0e1fee9377ede7669ce0204ad912cb69d5
Calvin Moore <c1moore@ufl.edu>
2015-02-08 10:56:56 -0500

    Added the events to the sidebars and changed the icon for 'Who's Here?'

--- public/js/script.js
+++ public/js/script.js
@@ -86,10 +86,12 @@ var appWindow = angular.module('appWindow', ['snap']);
 			restrict : "E",
 			template : 	"<a href='directory.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-home'></i> Home</a> <br />" +
 						"<a href='agenda.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-calendar'></i> Agenda</a> <br />" +
-						"<a href='frankbook_directory.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-user'></i> Who's Here?</a> <br />" +
+						"<a href='frankbook_directory.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-users'></i> Who's Here?</a> <br />" +
 						"<a href='frankbook.html' target='_self' class='btn btn-sidebar sidebar-item sidebar-item-2' role='button'><i class='fa fa-user'></i> frankbook</a> <br />" +
 						"<a href='frankbook_speakers.html' target='_self' class='btn btn-sidebar sidebar-item sidebar-item-2' role='button'><i class='fa fa-microphone'></i> Speaker frankbook</a> <br />" +
 						"<a href='./events' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-beer'></i> Events</a> <br />" +
+						"<a href='./events/event.html#1' target='_self' class='btn btn-sidebar sidebar-item sidebar-item-2' role='button'><i class='fa fa-share-alt'></i> Huddle 1</a> <br />" +
+						"<a href='./events/event.html#2' target='_self' class='btn btn-sidebar sidebar-item sidebar-item-2' role='button'><i class='fa fa-share-alt'></i> Huddle 2</a> <br />" +
 						"<a href='map.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-map-marker'></i> Map</a> <br />" +
 						"<a href='vote.html' target='_self' class='btn btn-sidebar sidebar-item' role='button'><i class='fa fa-check-square-o'></i> Voting</a> <br />",
 			link : function postlink() {
